d7b6a8ed5e7930cf9247667aca40f7cc700bed94
Gamareel <kmhasni15@gmail.com>
2022-12-15 20:01:36 +0100

    pushing

--- wL.gd
+++ wL.gd
@@ -3,7 +3,7 @@ extends Label
 
 # Declare member variables here. Examples:
 # var a = 2
-var wR = 0
+var wL = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,7 +13,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
-	if Input.is_action_just_pressed("ui_right"):
-		wR+=1;
-		text = str(wR);
+	if Input.is_action_just_pressed("ui_left"):
+		wL+=1;
+		text = str(wL);
