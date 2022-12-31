431480733324537304c2039ff77585e22995c5a3
Gamareel <kmhasni15@gmail.com>
2022-12-31 21:31:40 +0100

    pushed

--- Player/RobotMovement.gd
+++ Player/RobotMovement.gd
@@ -1,9 +1,8 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
 export (float) var rotation_speed = 1
 var velocity = Vector2()
-var rotation_dir = 0
+
 #############################################
 var delta_t = 0.1
 var dis_entre_Roue = 5 #cm
@@ -11,7 +10,7 @@ var ray_Robot = 5 #cm
 var ray_Roue = 2 #cm
 
 var delta_w_Max = 20 #rad/s
-var w_Max = 100 #rad/s
+var w_Max = 200 #rad/s
 var w_D = 0
 var w_G = 0
 
@@ -22,7 +21,6 @@ var rot_Robot = 0
 
 
 func get_input():
-	rotation_dir = 0
 	velocity = Vector2()
 	if Input.is_action_just_pressed("ui_right"):
 		w_G +=(delta_w_Max)
@@ -45,14 +43,15 @@ func _physics_process(delta):
 	get_input()
 	w_D = clamp(w_D,-w_Max,w_Max)
 	w_G = clamp(w_G,-w_Max,w_Max)
-	
+	$vitess_ang_G.text = str(w_G)
+	$vitess_ang_D.text = str(w_D)
 	dist_Droite = w_D * delta_t * ray_Roue
 	dist_Gauche = w_G * delta_t * ray_Roue
 	
 	dist_Robot = (dist_Droite + dist_Gauche)/2
-	rot_Robot = (dist_Gauche - dist_Droite)/(dis_entre_Roue) 
+	rot_Robot = (dist_Gauche - dist_Droite)/(dis_entre_Roue*30) 
 	
-	print(rot_Robot)
+	
 	#print("La vitesse de la roue droite est : ",w_D)
 	#print("La vitesse de la roue gauche est : ",w_G)
 	
@@ -60,11 +59,10 @@ func _physics_process(delta):
 	
 	if (dist_Robot!=0):
 		#print(dist_Robot)
-		#$wL.text = str(w_G)
-		#$wD.text = str(w_D)
+		
 		
 		velocity = (Vector2(dist_Robot,0)).rotated(rotation).clamped(w_Max)
-		
+		#print(velocity)
 		move_and_collide(velocity*delta)
 	if (dist_Robot==0):
 		move_and_collide(Vector2.ZERO * delta)
